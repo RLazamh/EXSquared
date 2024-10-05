fc8eb7bc42c751e9e7dfe77d05b288689d980203
RLazamh <roger.laza@epn.edu.ec>
2024-10-05 10:54:27 -0500

    add: Bottleneck for handle batch request

--- src/application/vehicle/process-vehicle-xml.usecase.ts
+++ src/application/vehicle/process-vehicle-xml.usecase.ts
@@ -1,30 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import { VehicleDomService } from '../../domain/vehicle/vehicle-dom.service';
+import Bottleneck from 'bottleneck';
 
 @Injectable()
 export class ProcessVehicleXmlUseCase {
   constructor(private readonly _vehicleDomService: VehicleDomService) {}
 
   async execute(): Promise<any[]> {
+    const limiter = new Bottleneck({
+      maxConcurrent: 30,
+      minTime: 50,
+    });
+
     const vehicles = await this._vehicleDomService.getVehicleMakeData();
-    const moreInfo = await this._vehicleDomService.getVehicleMakeByIdData(
-      vehicles[0].Make_ID,
-    );
+    const results = [];
 
-    const transformedData = [
-      {
-        makeId: vehicles[0].Make_ID.trim(),
-        makeName: vehicles[0].Make_Name.trim(),
-        vehicleTypes: [
-          {
-            typeId: moreInfo.VehicleTypeId.trim(),
-            typeName: moreInfo.VehicleTypeName.trim(),
-          },
-        ],
-      },
-    ];
+    for (const vehicle of vehicles) {
+      if (!vehicle || !vehicle.Make_ID || !vehicle.Make_Name) {
+        continue;
+      }
 
-    // Retornar los datos transformados
-    return transformedData;
+      const transformedVehicle = await limiter.schedule(async () => {
+        const moreInfo = await this._vehicleDomService.getVehicleMakeByIdData(
+          vehicle.Make_ID,
+        );
+
+        const vehicleTypesForMakeIds = Array.isArray(moreInfo)
+          ? moreInfo
+          : [moreInfo];
+
+        const vehicleTypes = vehicleTypesForMakeIds.map((type) => ({
+          typeId: type.VehicleTypeId?.trim(),
+          typeName: type.VehicleTypeName?.trim(),
+        }));
+
+        return {
+          makeId: vehicle.Make_ID.trim(),
+          makeName: vehicle.Make_Name.trim(),
+          vehicleTypes,
+        };
+      });
+
+      if (transformedVehicle) {
+        results.push(transformedVehicle);
+      }
+    }
+
+    return results;
   }
 }
