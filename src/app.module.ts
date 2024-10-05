c1e22240a53412feab54fa59a9fb773a52930561
RLazamh <roger.laza@epn.edu.ec>
2024-10-04 23:37:26 -0500

    add: layout of presentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { VehicleController } from './application/vehicle/vehicle.controller';
-import { VehicleService } from './domain/vehicle/vehicle.service';
-import { ProvidersModule } from './providers/providers.module';
+import { VehicleModule } from './presentation/vehicle/vehicle.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: 'dev.env',
     }),
-    ProvidersModule,
+    VehicleModule,
   ],
-  controllers: [VehicleController],
-  providers: [VehicleService],
 })
 export class AppModule {}
